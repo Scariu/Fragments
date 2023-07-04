87701ed704a7d43d9df07c309e13282d60d4cc73
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-03 22:13:00 -0400

    Se agrega binding a SecondFragment.

--- app/src/main/java/com/example/fragments/SecondFragment.java
+++ app/src/main/java/com/example/fragments/SecondFragment.java
@@ -8,13 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.fragments.databinding.FragmentFirstBinding;
+import com.example.fragments.databinding.FragmentSecondBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SecondFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class SecondFragment extends Fragment {
-
+    private FragmentSecondBinding binding;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -59,6 +62,7 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        binding = FragmentSecondBinding.inflate(getLayoutInflater(), container, false);
+        return binding.getRoot();
     }
 }
